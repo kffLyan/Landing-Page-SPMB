7031e5e532c122746168fca0ab510a5c334500a0
Kafka <kafkalyandra@gmail.com>
2026-05-25 20:11:56 +0700

    first upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js,php}", 
+    "./src/**/*.{html,js,jsx,ts,tsx,vue,php}", 
+    "./views/**/*.{html,js,php}" ],
+  theme: { extend: {} },
+  plugins: [],
+}
